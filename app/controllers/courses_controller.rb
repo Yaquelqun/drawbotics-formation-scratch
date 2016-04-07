bedd94725fe142dcb5e7f90f84ecf4e212b6ec7d
sandjiv <sandjiv.parassou@gmail.com>
2016-04-07 11:01:20 +0200

    add/show/index course(s) completed

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,22 @@
 class CoursesController < ApplicationController
 
+def index
+  @courses = Course.all
+end
+
+def new
+  @course = Course.new
+end
+
+def create
+  @course = Course.create(course_params)
+  redirect_to courses_path
+end
+
+private
+
+def course_params
+  params.require(:course).permit(:name)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ root 'static_pages#home'
 
 get 'addUser' => 'users#new'
 get 'listUsers' => 'users#index'
+get 'listCourses' => 'courses#index'
+get 'createCourse' => 'courses#new'
+
+
 resources :users
 resources :courses
 resources :chapters
