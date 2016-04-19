4af58e4300646db9c2f7e0274a882a5f3b578e51
sandjiv <sandjiv.parassou@gmail.com>
2016-04-19 14:19:51 +0200

    first tests written for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require 'support/controller_macros'
 require 'rails_helper'
 describe UsersController do
-  render_views
   login_user
 
   it "should have a current_user" do
@@ -17,16 +16,23 @@ describe ".index" do
   end
 end
 
+
 describe ".show" do
   it "gets the view" do
     get "show", id: subject.current_user.id
-      expect(response).to(be_success)
-
+    expect(response).to(be_success)
   end
 
-  it "has the right elements" do
+  it "recognises if it's my profile" do
     get "show", id: subject.current_user.id
-    expect(response).to(contain("Home"))
+    expect(subject.instance_variable_get(:@user_is_me)).to eql(true)
+  end
+
+  it "recognises if it isn't my profile" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    stranger = FactoryGirl.create(:user)
+    get "show", id: stranger.id
+    expect(subject.instance_variable_get(:@user_is_me)).to eql(false)
   end
 end
 
